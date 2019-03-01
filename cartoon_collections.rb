115e438f76d49baae2fd3d3621ceb8653e71bafd
Jared Grove <jaredkgrove@gmail.com>
2019-03-01 07:05:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,11 +17,14 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   end
 end
 
+
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-    
+  ingredients.find do |cheese|
+    cheese_types.include?(cheese)
   end
+  
       
 end
